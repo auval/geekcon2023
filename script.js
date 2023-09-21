7a305668432c26804fe650f1c0ec2fea56105e64
auval@mycwt.com <auval@mycwt.com>
2023-09-21 21:38:52 +0300

    adding html+js

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+function fetchResponse() {
+    const apiKey = document.getElementById('apiKey').value;
+    const prompt = document.getElementById('prompt').value;
+
+    const url = "https://api.openai.com/v1/engines/davinci/completions";
+    const body = {
+        prompt: prompt,
+        max_tokens: 150
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const text = data.choices && data.choices[0] && data.choices[0].text;
+        document.getElementById('response').innerText = text;
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        document.getElementById('response').innerText = "Error: " + error.message;
+    });
+}
