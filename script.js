ad60c5c601d1937bb734e004b1784b06be2316ad
Oded Sharon <git@odedsharon.com>
2023-09-22 01:44:43 +0300

    choices?

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function fetchResponse() {
     .then(response => response.json())
     .then(data => {
         console.log("data"  + data);
-        console.log("data.choices"  + data.choices[0]);
+        console.log("data.choices"  + data.choices);
         const text = data.choices && data.choices[0] && data.choices[0].text;
         document.getElementById('response').innerText = text;
     })
