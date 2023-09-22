f5a1eaea1254af3b2a3cd185916e275aa62fa352
auval@mycwt.com <auval@mycwt.com>
2023-09-22 14:23:43 +0300

    fix dali

--- dali.js
+++ dali.js
@@ -1,12 +1,12 @@
 async function generateImage() {
-    const endpoint = "https://api.openai.com/v1/engines/dali/completions"; // This is a hypothetical endpoint; the actual one might differ.
+    const endpoint = "https://api.openai.com/v1/images/generations";
     const apiKey = document.getElementById('apiKey').value;
     const promptText = document.getElementById('prompt').value;
-    // const prefix = document.getElementById('prefix').value; // This line is unused in the current function.
 
     const requestBody = {
         prompt: promptText,
-        // Add any other necessary parameters here
+        n: 1,
+        size: "1024x1024"
     };
 
     try {
@@ -19,15 +19,15 @@ async function generateImage() {
             body: JSON.stringify(requestBody)
         });
 
-        if (!response.ok) {
-            throw new Error(`API call failed with status ${response.status}`);
-        }
-
         const data = await response.json();
 
-        // Assuming the response contains a direct link to the generated image
-        const imageUrl = data.image_url; // This is hypothetical; the actual key might differ.
+        // Handle the response data as needed
+        console.log(data);
 
+        // If the response contains a direct link to the generated image:
+        const imageUrl = data.data[0].url; // Adjust this based on the actual response structure
+
+        // Display the image in your HTML (if desired)
         const imgElement = document.createElement("img");
         imgElement.src = imageUrl;
         document.body.appendChild(imgElement);
@@ -35,27 +35,9 @@ async function generateImage() {
         return imageUrl;
     } catch (error) {
         console.error("Error generating image:", error);
-        alert("Error generating image: " + error.message);
     }
 }
 
-// Add an event listener to the button
-//document.getElementById('generateImageButton').addEventListener('click', generateImage);
-
-
-/*
-// Usage example:
-generateImage("A sunset over a mountain range", "YOUR_OPENAI_API_KEY")
-    .then(imageUrl => {
-        // Display the image in your HTML
-        const imgElement = document.createElement("img");
-        imgElement.src = imageUrl;
-        document.body.appendChild(imgElement);
-    })
-    .catch(error => {
-        console.error("Error generating image:", error);
-    });
-*/
 
 // Retrieve the API key from the cookie when the page loads
 window.addEventListener('load', function () {
